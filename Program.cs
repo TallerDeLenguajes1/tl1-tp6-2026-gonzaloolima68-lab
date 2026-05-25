5459e0601ede41133ecec79bc3b99d4622a6bcc9
Gonzalo Olima <gonzaloolima68@gmail.com>
2026-05-25 20:37:52 -0300

    ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string ingresado;
+Console.WriteLine("Ingrese un numero");
+ingresado=Console.ReadLine();
+
+
+if(int.TryParse(ingresado,out int num))
+{
+    int num2=num;
+    int invertido=0;
+    if (num > 0)
+    {
+        while (num != 0)
+        {
+            int digito=num%10;
+            invertido=invertido*10+digito;
+            num=num/10;
+        }
+        Console.WriteLine("el numero ingresado es "+num2);
+        Console.WriteLine("el numero invertido es "+invertido);
+    }
+    else
+    {
+        Console.WriteLine("El numero ingresado debe ser mayor a cero");
+    }
+}
+else
+{
+    Console.WriteLine("No se ingreso un numero valido" );
+}
+
