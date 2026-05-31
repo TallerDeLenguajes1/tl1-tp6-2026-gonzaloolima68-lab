ff0ba086bb2e6fe91d5ee13290902358bfab799a
Gonzalo Olima <gonzaloolima68@gmail.com>
2026-05-31 01:00:55 -0300

    menu de seleccion

--- Program.cs
+++ Program.cs
@@ -1,30 +1,30 @@
-﻿string ingresado;
-Console.WriteLine("Ingrese un numero");
-ingresado=Console.ReadLine();
+﻿using System;
+using System.Security.AccessControl;
 
+char continuar;
 
-if(int.TryParse(ingresado,out int num))
+do
 {
-    int num2=num;
-    int invertido=0;
-    if (num > 0)
+    Console.WriteLine("Ingrese la operacion a realizar");
+    Console.WriteLine("1-sumar");
+    Console.WriteLine("2-restar");
+    Console.WriteLine("3-multiplicar");
+    Console.WriteLine("4-restar");
+    Console.WriteLine("5-terminar");
+
+    Console.WriteLine("Ingrese el primer numero");
+    double num1;
+    while(!double.TryParse(Console.ReadLine(),out num1))
     {
-        while (num != 0)
-        {
-            int digito=num%10;
-            invertido=invertido*10+digito;
-            num=num/10;
-        }
-        Console.WriteLine("el numero ingresado es "+num2);
-        Console.WriteLine("el numero invertido es "+invertido);
+        Console.WriteLine("Numero invalido intente nuevamente");
     }
-    else
+    Console.WriteLine("Ingrese el segundo numero");
+    double num2;
+    while(!double.TryParse(Console.ReadLine(),out num2))
     {
-        Console.WriteLine("El numero ingresado debe ser mayor a cero");
+        Console.WriteLine("Numero invalido intente nuevamente");
     }
-}
-else
-{
-    Console.WriteLine("No se ingreso un numero valido" );
-}
+    
+    continuar=Convert.ToChar(Console.ReadLine().ToLower());
+} while (continuar == 's');
 
